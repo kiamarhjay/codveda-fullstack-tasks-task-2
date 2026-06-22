097d9b1174c3635adb5d28a4e3e0835e459aadd7
kiamarhjay <jibrilqiyaamah@gmail.com>
2026-06-22 20:49:01 +0300

    Feat: Initialize the backend MVC folder structure and configuration

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config(); //loads environment files from process.env
+connectDB(); //connects to mongoDB database
+const app = express();
+
+//global middlewares
+app.use(cors()); //allow frontend framework to talk to backend server using different port
+app.use(express.json()); //allow express routes to read json body data sent by clients 
+app.get('/', (req,res) => {
+    res.send('API is running successfully 🌿');
+});
+
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server blasting off on port ${PORT}`));
